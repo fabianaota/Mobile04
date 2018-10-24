c1c3a83795cab7e792eb93e2db076956d87e1ad3
fabiotadashi <miyasato.fabio@gmail.com>
2018-10-24 09:58:37 -0300

    view pager app

---
+++ ViewPagerApp/app/src/main/java/com/digitalhouse/viewpagerapp/adapter/ColorViewPagerAdapter.java
@@ -0,0 +1,28 @@
+package com.digitalhouse.viewpagerapp.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+public class ColorViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    private List<Fragment> fragmentList;
+
+    public ColorViewPagerAdapter(FragmentManager fm, List<Fragment> fragmentList) {
+        super(fm);
+        this.fragmentList = fragmentList;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+}
