d4e06cd4dd5156e807f4116f602f1cb98251bd72
fabiotadashi <miyasato.fabio@gmail.com>
2018-10-31 22:19:42 -0300

    search view implementation

--- AgendaDigitalhouse/app/src/main/java/com/digitalhouse/agendadigitalhouse/MainActivity.java
+++ AgendaDigitalhouse/app/src/main/java/com/digitalhouse/agendadigitalhouse/MainActivity.java
@@ -1,5 +1,7 @@
 package com.digitalhouse.agendadigitalhouse;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,15 +10,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.digitalhouse.agendadigitalhouse.adapter.RecyclerViewAgendaAdapter;
 import com.digitalhouse.agendadigitalhouse.model.Contato;
 
-public class MainActivity extends AppCompatActivity implements CriarContatoFragment.CriarContatoActions {
+public class MainActivity extends AppCompatActivity implements CriarContatoFragment
+        .CriarContatoActions {
 
     private RecyclerViewAgendaAdapter adapter;
     private CriarContatoFragment fragment;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +45,58 @@ public class MainActivity extends AppCompatActivity implements CriarContatoFragm
                 transaction.commit();
             }
         });
+
+        Toolbar toolbar = findViewById(R.id.toolbar_id);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        toolbar.setTitle("Agenda de telefones");
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.action_search)
+                .getActionView();
+        searchView.setSearchableInfo(searchManager
+                .getSearchableInfo(getComponentName()));
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+
+        // listening to search query text change
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // filter recycler view when query submitted
+                adapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                // filter recycler view when text is changed
+                adapter.getFilter().filter(query);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+
+                return true;
+            case R.id.action_clear:
+                adapter.removeAll();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 
     private void setupRecyclerView() {
--- AgendaDigitalhouse/app/src/main/java/com/digitalhouse/agendadigitalhouse/adapter/RecyclerViewAgendaAdapter.java
+++ AgendaDigitalhouse/app/src/main/java/com/digitalhouse/agendadigitalhouse/adapter/RecyclerViewAgendaAdapter.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import com.digitalhouse.agendadigitalhouse.R;
@@ -14,9 +16,10 @@ import com.digitalhouse.agendadigitalhouse.model.Contato;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RecyclerViewAgendaAdapter extends RecyclerView.Adapter<RecyclerViewAgendaAdapter.ViewHolder> {
+public class RecyclerViewAgendaAdapter extends RecyclerView.Adapter<RecyclerViewAgendaAdapter.ViewHolder> implements Filterable {
 
     private List<Contato> contatoList = new ArrayList<>();
+    private List<Contato> filteredList = new ArrayList<>();
 
     @NonNull
     @Override
@@ -27,18 +30,55 @@ public class RecyclerViewAgendaAdapter extends RecyclerView.Adapter<RecyclerView
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-        Contato contato = contatoList.get(i);
+        Contato contato = filteredList.get(i);
         viewHolder.bind(contato);
     }
 
     @Override
     public int getItemCount() {
-        return contatoList.size();
+        return filteredList.size();
     }
 
     public void addContato(Contato contato){
         contatoList.add(contato);
+        filteredList.add(contato);
         notifyItemChanged(getItemCount());
+    }
+
+    public void removeAll() {
+        contatoList.clear();
+        filteredList.clear();
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                filteredList = new ArrayList<>();
+                String search = charSequence.toString();
+                if(search.isEmpty()){
+                    filteredList.addAll(contatoList);
+                }else{
+                    for (Contato contato : contatoList) {
+                        if(contato.getNome().contains(search)){
+                            filteredList.add(contato);
+                        }
+                    }
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = filteredList;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                filteredList = (List<Contato>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
