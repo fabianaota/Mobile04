acc4a6bd4a33afc71aedd40ea6c549e471844ca0
Fabio Tadashi Miyasato <fabiotadashi@MacBook-Pro-de-Fabio.local>
2018-11-20 22:07:00 -0200

    fix login

--- FirebaseApp/app/src/main/java/com/digitalhouse/firebaseapp/LoginActivity.java
+++ FirebaseApp/app/src/main/java/com/digitalhouse/firebaseapp/LoginActivity.java
@@ -190,7 +190,7 @@ public class LoginActivity extends AppCompatActivity {
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            Log.e(TAG, "signInResult:failed code=" + e.getStatusCode());
         }
     }
 
